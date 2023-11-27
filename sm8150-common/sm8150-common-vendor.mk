35321e5daf4f85112c8545674299eb5b0cc86699
DarkJoker360 <simoespo159@gmail.com>
2023-11-27 10:09:55 +0100

    sm8150-common: Sync with device tree
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- sm8150-common/sm8150-common-vendor.mk
+++ sm8150-common/sm8150-common-vendor.mk
@@ -953,5 +953,4 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-service-qti \
     android.hardware.neuralnetworks@1.3-service-qti-hta \
     android.hardware.neuralnetworks@1.3-service-qti \
-    manifest_android.hardware.drm@1.3-service.widevine \
     vendor.qti.gnss@4.0-service
